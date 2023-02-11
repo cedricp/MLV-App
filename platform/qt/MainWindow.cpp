2ecf35c6cccb632a547564e600355aa8febf895d
anibarro <eduardo.anibarro@gmail.com>
2023-02-11 13:42:26 +0100

    Do not set best settings for Dual ISO on activation

--- platform/qt/MainWindow.cpp
+++ platform/qt/MainWindow.cpp
@@ -8490,11 +8490,11 @@ void MainWindow::toolButtonDualIsoChanged( void )
         ui->checkBoxDualIsoHorizontalStripesFix->setEnabled( false );
     }
 
-    if (toolButtonDualIsoCurrentIndex() > 0){
-        setToolButtonBadPixelsIntMethod(1); //Best for Dual ISO
-        setToolButtonFocusPixelsIntMethod(2); //Best for Dual ISO
-        setToolButtonBadPixels(3); //Only method really working for Dual ISO and preferred in any mode anyways
-    }
+//    if (toolButtonDualIsoCurrentIndex() > 0){
+//        setToolButtonBadPixelsIntMethod(1); //Best for Dual ISO
+//        setToolButtonFocusPixelsIntMethod(2); //Best for Dual ISO
+//        setToolButtonBadPixels(3); //Only method really working for Dual ISO and preferred in any mode anyways
+//    }
 
     //Set dualIso mode
     llrpSetDualIsoMode( m_pMlvObject, toolButtonDualIsoCurrentIndex() );
